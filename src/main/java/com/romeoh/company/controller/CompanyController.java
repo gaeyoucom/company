a2fcc0cddfb25a9bcbb765dce8364fae6e77cbc1
romeoh <romeoh78@gmail.com>
2021-03-21 23:46:01 +0900

    logback 설정

--- src/main/java/com/romeoh/company/controller/CompanyController.java
+++ src/main/java/com/romeoh/company/controller/CompanyController.java
@@ -2,6 +2,8 @@ package com.romeoh.company.controller;
 
 import com.romeoh.company.model.Company;
 import com.romeoh.company.service.CompanyService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,6 +20,14 @@ public class CompanyController {
     // http://localhost:9988/
     @GetMapping("/")
     public Hashtable<String, Company> all() {
+        Logger logger = LoggerFactory.getLogger(CompanyController.class);
+
+        logger.trace("trace - Hello world");
+        logger.debug("debug - Hello world");
+        logger.info("info - Hello world");
+        logger.warn("warn - Hello world");
+        logger.error("error - Hello world");
+
         return companyService.all();
     }
 
